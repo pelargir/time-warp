126be7486f4e7acd836215dfee656812fd43b1ce
Matthew Bass <pelargir@173-6-55-46.pools.spcsdns.net>
2009-05-26 11:49:49 -0400

    fixed naming bug

--- lib/time_warp.rb
+++ lib/time_warp.rb
@@ -4,7 +4,7 @@ module TimeWarp
   def pretend_now_is(*args)
     begin
       time_from = (args.size == 1 && args[0].is_a?(Time)) ? args[0] : Time.utc(*args)
-      Time.testing_offset = Time.now - time_from(*args)
+      Time.testing_offset = Time.now - time_from
       yield
     ensure
       Time.testing_offset = 0
